66302889cdbf89861ae818ef6e85ebc39ef817b4
Christian Stier <christian.stier@amotiq.de>
2026-04-10 09:12:59 +0200

    Dark Mode Test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,34 +1,20 @@
-﻿using NFL_Quiz.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Media;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Path = System.IO.Path;
+﻿using MahApps.Metro.Controls;
+using NFL_Quiz.ViewModel;
+
 
 namespace NFL_Quiz
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         
 
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using NFL_Quiz.MVVM;
+using System.Printing;
 using System.Windows.Input;
 
 namespace NFL_Quiz.ViewModel
@@ -6,13 +7,25 @@ namespace NFL_Quiz.ViewModel
     internal class MainWindowViewModel : ViewModelBase
     {
         private object _currentView;
-        private object CurrentView
+        public object CurrentView
         {
             get => _currentView;
             set
             {
                 _currentView = value;
                 OnPropertyChanged();
+            }
+        }
+
+        private bool _isDarkMode;
+        public bool IsDarkMode
+        {
+            get => _isDarkMode;
+            set
+            {
+                _isDarkMode = value;
+                OnPropertyChanged();
+                UpdateTheme(value);
             }
         }
 
@@ -26,5 +39,23 @@ namespace NFL_Quiz.ViewModel
             ShowGameCommand = new RelayCommand(o => CurrentView = new GamePage());
             ShowSettingsCommand = new RelayCommand(o => CurrentView = new SettingsPage());
         }
+
+        private void UpdateTheme(bool isDarkMode)
+        {
+            var app = System.Windows.Application.Current;
+            if (isDarkMode)
+            {
+                app.Resources.MergedDictionaries.Clear();
+                app.Resources.MergedDictionaries.Add(new System.Windows.ResourceDictionary { Source = new Uri("pack://application:,,,/Themes/DarkMode.xaml") });
+            }
+            else
+            {
+                app.Resources.MergedDictionaries.Clear();
+                app.Resources.MergedDictionaries.Add(new System.Windows.ResourceDictionary { Source = new Uri("pack://application:,,,/Themes/LightMode.xaml") });
+            }
+        }
+
+
+
     }
 }
